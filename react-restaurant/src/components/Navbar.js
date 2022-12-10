805593c0d2b0dd87c435357dcb5aa37c580acc67
Ishmael M. Dukuly <moisemadukuly82@gmail.com>
2022-12-10 19:16:11 +0000

    navbar design

---
+++ react-restaurant/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+export default function Navbar() {
+    return(
+        <>
+        <div className="navbar">
+         <ul>
+            <li><a href="/Home">Home</a></li>
+            <li><a href="/Home">Online Puchase</a></li>
+            <li><a href="/Home">Restaurants</a></li>
+            <li><a href="/Home">Contact Us</a></li>
+         </ul>
+        </div>
+        </>
+    );
+}
